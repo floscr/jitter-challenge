71ec5043f6f7dae6da1fd5afd3f8a5762f5c49f8
Florian Schroedl <flo.schroedl@gmail.com>
2024-01-25 11:06:44 +0100

    Remove explicit any lint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,6 @@ module.exports = {
       "warn",
       { allowConstantExport: true },
     ],
-    "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": [
       "warn",
       {
